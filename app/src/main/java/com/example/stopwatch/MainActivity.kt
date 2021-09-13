913d4fa486088561b6a9340d73e5637219a793d1
boden <bodenharlowe21@gmail.com>
2021-09-13 10:10:05 -0700

    added lifecycles finished timer

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -3,15 +3,53 @@ package com.example.stopwatch
 import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
+import android.widget.Button
+import android.widget.Chronometer
+import android.widget.Switch
+import androidx.constraintlayout.widget.ConstraintLayout
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     companion object{
         val TAG = "MainActivity"
     }
+    lateinit var startButton: Button
+    lateinit var resetButton: Button
+    lateinit var timer:Chronometer
+    lateinit var layout: ConstraintLayout
+    var Switched: Boolean = false
+    var time = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        wireWidgets()
+
+    startButton.setOnClickListener{
+        if(Switched){
+            timer.stop()
+            time = SystemClock.elapsedRealtime() - timer.base
+            startButton.text = "Resume"
+
+        }
+        else{
+            timer.start()
+            timer.base = SystemClock.elapsedRealtime() - time
+            startButton.text = "stop"
+        }
+        Switched = !Switched
+    }
+
+    resetButton.setOnClickListener {
+        if(Switched){
+            startButton.callOnClick()
+        }
+        time = 0L
+        timer.base = SystemClock.elapsedRealtime()
+        Switched = !Switched
+    }
+
     }
 
     override fun onPause() {
@@ -42,5 +80,13 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy: ")
+    }
+
+
+
+    private fun wireWidgets(){
+    startButton = findViewById(R.id.Start)
+    resetButton = findViewById(R.id.Reset)
+    timer = findViewById(R.id.chronometer_main_stopwatch)
     }
 }
