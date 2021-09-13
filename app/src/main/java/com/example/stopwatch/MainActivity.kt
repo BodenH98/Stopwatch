12fd7da17e68f79ca6492d26916c9cc46fb67bb2
boden <bodenharlowe21@gmail.com>
2021-09-13 08:51:33 -0700

    added lifecycles

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -18,4 +18,29 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
         Log.d(TAG, "onPause: ")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
 }
